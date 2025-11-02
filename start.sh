701f70682b8fafe5c53a778b66b9a7b61938f8fb
Harsha-2515 <harsha.intern@thinkinfinity.co.in>
2025-11-02 22:48:12 +0530

    change in start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-set -e
-
-# Start Rasa action server in the background
-cd actions
-rasa run actions --port 5055 --cors "*" &
-cd ..
-
-# Get Render's PORT or fallback to 5005
-PORT_TO_USE=${PORT:-5005}
-
-# Start Rasa server properly
-rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT_TO_USE}
+# This command starts ONLY the action server.
+rasa run actions --port $PORT --cors "*"
